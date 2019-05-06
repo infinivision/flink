d8de220a31b1809a8d96ec7932611aa23bbfa95e
zhangfeng <smilebackxiaofeng@gmail.com>
2019-05-06 12:39:27 +0800

    update job handler

--- flink-runtime/src/main/java/org/apache/flink/runtime/rest/handler/job/update/UpdateJobTriggerHeaders.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/rest/handler/job/update/UpdateJobTriggerHeaders.java
@@ -57,7 +57,7 @@ public class UpdateJobTriggerHeaders extends
 
 	@Override
 	public HttpMethodWrapper getHttpMethod() {
-		return HttpMethodWrapper.PATCH;
+		return HttpMethodWrapper.GET;
 	}
 
 	@Override
